97086650ed7649fd0dc316d587074375d3b4d73b
daryaleontyeva <36742645+daryaleontyeva@users.noreply.github.com>
2018-05-06 22:43:18 +0500

    Добавьте файлы проекта.

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Security.Cryptography;
+
+namespace License
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Name, Path = @"C:\\Users\\Астемир\\Desktop\\StudentCard"; //путь сохранения файла лицензии
+            DateTime StartDate, End;
+            Name = textBox_FIO.Text;
+            StartDate = Convert.ToDateTime(DatePicker_Start.SelectedDate);
+            End = Convert.ToDateTime(DatePicker_End.SelectedDate);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(@" <license>
+            <Name></Name>
+            <Date></Date>
+            <UpdateTo></UpdateTo>
+            <Signature></Signature>
+            </license>");
+                doc.ChildNodes[0].SelectSingleNode(@"/license/Name", null).InnerText = Name;
+                doc.ChildNodes[0].SelectSingleNode(@"/license/Date", null).InnerText = StartDate.ToShortDateString();
+                doc.ChildNodes[0].SelectSingleNode(@"/license/UpdateTo", null).InnerText = End.ToShortDateString();
+
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(Name + StartDate.ToShortDateString() + End.ToShortDateString() + "SomePasswordKey");
+                byte[] hash = md5.ComputeHash(data);
+                doc.ChildNodes[0].SelectSingleNode(@"/license/Signature", null).InnerText = Convert.ToBase64String(hash);
+                doc.Save(System.IO.Path.Combine(Path, "license.xml"));
+            MessageBox.Show("Лизензия для приложения " + Name + " сохранена в " + Path + "\nИ будет действовать с " + StartDate + " до " + End);
+        }
+
+    }
+}
---
+++ WpfApplication1/data.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WpfApplication1
+{
+    [Serializable, XmlInclude(typeof(DataBaseForStudents))]
+    public class DataBaseForStudents
+    {
+        public DataBaseForStudents() { }
+        public DataBaseForStudents(string fio, string age, string fak, string game, int skill, int course, string twitch, string expirience)
+        {
+            this.fio = fio;
+            this.age = age;
+            this.fak = fak;
+            this.game = game;
+            this.skill = skill;
+            this.course = course;
+            this.twitch = twitch;
+            this.expirience = expirience;
+        }
+        public string fio { get; set; }
+        public string age { get; set; }
+        public string fak { get; set; }
+        public string game { get; set; }
+        public int skill { get; set; }
+        public int course { get; set; }
+        public string twitch { get; set; }
+        public string expirience { get; set; }
+    }
+}
